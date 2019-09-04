24da7cd16cbc4e4d850a63768e2fe142ff4e4e60
meer sawood <sawoodrocket@gmail.com>
2019-09-05 01:28:46 +0530

    login

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:bloc/bloc.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:ecodrive/authentication_bloc/bloc.dart';
-import 'package:ecodrive/user_repository.dart';
-import 'package:ecodrive/home_screen.dart';
-import 'package:ecodrive/login/login.dart';
-import 'package:ecodrive/splash_screen.dart';
-import 'package:ecodrive/simple_bloc_delegate.dart';
 
-void main() {
-  BlocSupervisor.delegate = SimpleBlocDelegate();
-  final UserRepository userRepository = UserRepository();
-  runApp(
-    BlocProvider(
-      builder: (context) => AuthenticationBloc(userRepository: userRepository)
-        ..dispatch(AppStarted()),
-      child: App(userRepository: userRepository),
-    ),
-  );
-}
+import 'package:ecodrive/pages/root_page.dart';
+import 'package:ecodrive/services/authentication.dart';
 
-class App extends StatelessWidget {
-  final UserRepository _userRepository;
+void main() => runApp(MyApp());
 
-  App({Key key, @required UserRepository userRepository})
-      : assert(userRepository != null),
-        _userRepository = userRepository,
-        super(key: key);
-
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        brightness: Brightness.light,
-        primaryColor: Colors.pink[300],
-      ),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-      ),
-      home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
-        builder: (context, state) {
-          if (state is Unauthenticated) {
-            return LoginScreen(userRepository: _userRepository);
-          }
-          if (state is Authenticated) {
-            return HomeScreen(name: state.displayName);
-          }
-          return SplashScreen();
-        },
-      ),
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.pink,
+        ),
+        home: new RootPage(auth: new Auth())
     );
+
   }
 }
