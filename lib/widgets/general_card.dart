f6f32b7b68d9725bc42832a726edc43912742601
Piyush Mehta <me@piyushmehta.com>
2019-09-04 23:58:36 +0530

    card

---
+++ lib/widgets/general_card.dart
@@ -0,0 +1,163 @@
+import 'package:flutter/material.dart';
+import "animated_fab.dart";
+import "energy_clipper.dart";
+
+class GeneralCard extends StatefulWidget {
+  final String title;
+  final String date;
+  final double kw;
+
+  GeneralCard({this.title, this.date, this.kw});
+  @override
+  _GeneralCardState createState() => new _GeneralCardState();
+}
+
+class _GeneralCardState extends State<GeneralCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Card(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+          elevation: 2,
+          child: Stack(
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  Expanded(
+                    flex: 2,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Color(0xFFCF2156),
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(8),
+                          topRight: Radius.circular(8),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(8),
+                          bottomRight: Radius.circular(8),
+                        ),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            "Max speed 45Kmph",
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 10,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 20.0, top: 5),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: <Widget>[
+                                Text(
+                                  " Rs",
+                                  style: TextStyle(
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  "${widget.kw}",
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 15.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        "${widget.title}",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        "${widget.date}",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white54,
+                          fontWeight: FontWeight.normal,
+                          fontSize: 12,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 0.0,
+                right: 0.0,
+                bottom: 75.0,
+                child: ClipPath(
+                  clipper: EnergyRateClipper(),
+                  child: Container(
+                    height: 70.0,
+                    decoration: BoxDecoration(
+                      color: Colors.pink,
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 0.0,
+                right: 0.0,
+                bottom: 70.0,
+                child: ClipPath(
+                  clipper: EnergyRateClipper(),
+                  child: Container(
+                    height: 50.0,
+                    decoration: BoxDecoration(
+                      color: Colors.white70,
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 0,
+                right: -110,
+                bottom: 18,
+                child: AnimatedFab(
+                  onClick: () => print("clicked"),
+                ),
+              )
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
