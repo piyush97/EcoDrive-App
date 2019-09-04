b771239792d8b4ed34ce0f2bcd549fa2b22d297b
Piyush Mehta <me@piyushmehta.com>
2019-09-05 00:21:07 +0530

    bikes shown

--- lib/pages/bikes.dart
+++ lib/pages/bikes.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import "../widgets/general_card.dart";
+import "../main.dart";
 
 class EnergyCardPage extends StatefulWidget {
   @override
@@ -10,56 +11,74 @@ class _EnergyCardPageState extends State<EnergyCardPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "Book your Ecodrive!",
-            style: TextStyle(color: Colors.white),
-          ),
-          backgroundColor: Colors.pink,
+      appBar: AppBar(
+        title: Text(
+          "Book your Ecodrive!",
+          style: TextStyle(color: Colors.white),
         ),
-        drawer: new Drawer(
-          child: ListView(
-            children: <Widget>[
-              new UserAccountsDrawerHeader(
-                accountName: new Text('Piyuh Mehta'),
-                accountEmail: new Text('testemail@test.com'),
-                currentAccountPicture: new CircleAvatar(
-                  backgroundImage: new NetworkImage('http://i.pravatar.cc/400'),
-                ),
+        backgroundColor: Colors.pink,
+      ),
+      drawer: new Drawer(
+        child: ListView(
+          children: <Widget>[
+            new UserAccountsDrawerHeader(
+              accountName: new Text('Piyush Mehta'),
+              accountEmail: new Text('testemail@test.com'),
+              currentAccountPicture: new CircleAvatar(
+                backgroundImage: new NetworkImage('http://i.pravatar.cc/300'),
               ),
-            ],
-          ),
+            ),
+            new ListTile(
+              title: new Text('Log Out'),
+              onTap: () {
+                Navigator.of(context).pop();
+                Navigator.push(
+                    context,
+                    new MaterialPageRoute(
+                        builder: (BuildContext context) => new MyApp()));
+              },
+            ),
+          ],
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: GridView.count(
-            childAspectRatio: 0.7,
-            crossAxisCount: 2,
-            crossAxisSpacing: 5,
-            mainAxisSpacing: 5,
-            children: <Widget>[
-              GeneralCard(
-                title: "Bike 1",
-                date: "Available for 2 days",
-                kw: 4900,
-              ),
-              GeneralCard(
-                title: "Bike 2",
-                date: "Available for 2 days",
-                kw: 4200,
-              ),
-              GeneralCard(
-                date: "Available for 2 days",
-                title: "Bike 3",
-                kw: 2300,
-              ),
-              GeneralCard(
-                title: "Bike 4",
-                date: "Available for 2 days",
-                kw: 2300,
-              ),
-            ],
-          ),
-        ));
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GridView.count(
+          childAspectRatio: 0.7,
+          crossAxisCount: 2,
+          crossAxisSpacing: 5,
+          mainAxisSpacing: 5,
+          children: <Widget>[
+            GeneralCard(
+              title: "Bike 1",
+              date: "Available for 2 days",
+              kw: 4900,
+            ),
+            GeneralCard(
+              title: "Bike 2",
+              date: "Available for 2 days",
+              kw: 4200,
+            ),
+            GeneralCard(
+              date: "Available for 2 days",
+              title: "Bike 3",
+              kw: 2300,
+            ),
+            GeneralCard(
+              title: "Bike 4",
+              date: "Available for 2 days",
+              kw: 2300,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Add your onPressed code here!
+        },
+        child: Text("Repair"),
+        backgroundColor: Colors.pink,
+      ),
+    );
   }
 }
