24fc919060a3122dc3dcb2e5e4c267fee47bc855
Piyush Mehta <me@piyushmehta.com>
2019-09-09 22:38:46 +0530

    verify profile

--- lib/pages/verify_profile.dart
+++ lib/pages/verify_profile.dart
@@ -1,118 +1,178 @@
-import 'package:ecodrive/pages/bikes.dart';
-import 'package:ecodrive/pages/rides.dart';
-import "package:flutter/material.dart";
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:file_picker/file_picker.dart';
+import 'dart:io';
+import 'package:flutter/material.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/widgets.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:image_picker/image_picker.dart';
 
-class VerifyProfile extends StatelessWidget {
-  FirebaseAuth _auth = FirebaseAuth.instance;
+/// Widget to capture and crop the image
+class VerifyProfile extends StatefulWidget {
+  createState() => _VerifyProfileState();
+}
+
+class _VerifyProfileState extends State<VerifyProfile> {
+  /// Active image file
+  File _imageFile;
+
+  /// Cropper plugin
+  Future<void> _cropImage() async {
+    File cropped = await ImageCropper.cropImage(
+        sourcePath: _imageFile.path,
+        // ratioX: 1.0,
+        // ratioY: 1.0,
+        // maxWidth: 512,
+        // maxHeight: 512,
+        toolbarColor: Colors.purple,
+        toolbarWidgetColor: Colors.white,
+        toolbarTitle: 'Crop It');
+
+    setState(() {
+      _imageFile = cropped ?? _imageFile;
+    });
+  }
+
+  /// Select an image via gallery or camera
+  Future<void> _pickImage(ImageSource source) async {
+    File selected = await ImagePicker.pickImage(source: source);
+
+    setState(() {
+      _imageFile = selected;
+    });
+  }
+
+  /// Remove image
+  void _clear() {
+    setState(() => _imageFile = null);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Verify your Identity!",
-          style: TextStyle(color: Colors.white),
-        ),
-        backgroundColor: Colors.pink,
-      ),
-      drawer: new Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            UserAccountsDrawerHeader(
-              accountName: new Text('Piyush Mehta'),
-              accountEmail: new Text('testemail@test.com'),
-              currentAccountPicture: new CircleAvatar(
-                backgroundImage: new NetworkImage('http://i.pravatar.cc/300'),
+            IconButton(
+              icon: Icon(
+                Icons.photo_camera,
+                size: 30,
               ),
+              onPressed: () => _pickImage(ImageSource.camera),
+              color: Colors.blue,
             ),
-            ListTile(
-              title: new Text('Book your ride'),
-              onTap: () {
-                Navigator.of(context).pop();
-                Navigator.push(
-                    context,
-                    new MaterialPageRoute(
-                        builder: (BuildContext context) => new BikesPage()));
-              },
-            ),
-            ListTile(
-              title: new Text('Verify your Profile'),
-              onTap: () {
-                Navigator.of(context).pop();
-                Navigator.push(
-                    context,
-                    new MaterialPageRoute(
-                        builder: (BuildContext context) =>
-                            new VerifyProfile()));
-              },
-            ),
-            ListTile(
-              title: new Text('Loyalty Programme'),
-              onTap: () {
-                Navigator.of(context).pop();
-                Navigator.push(
-                    context,
-                    new MaterialPageRoute(
-                        builder: (BuildContext context) => new Rides()));
-              },
-            ),
-            ListTile(
-              title: new Text('Complaint Box'),
-              onTap: () {
-                Navigator.of(context).pop();
-                Navigator.push(
-                    context,
-                    new MaterialPageRoute(
-                        builder: (BuildContext context) => new Rides()));
-              },
-            ),
-            ListTile(
-              title: new Text('Repair'),
-              onTap: () {
-                Navigator.of(context).pop();
-                Navigator.push(
-                    context,
-                    new MaterialPageRoute(
-                        builder: (BuildContext context) => new Rides()));
-              },
-            ),
-            ListTile(
-              title: new Text(
-                'Help?',
-                style: TextStyle(color: Colors.pink.withOpacity(1.0)),
+            IconButton(
+              icon: Icon(
+                Icons.photo_library,
+                size: 30,
               ),
-              onTap: () {
-                Navigator.of(context).pop();
-                Navigator.push(
-                    context,
-                    new MaterialPageRoute(
-                        builder: (BuildContext context) => new Rides()));
-              },
-            ),
-            new Container(
-              height: 1,
-              width: 1,
-              color: Colors.grey,
-              margin: const EdgeInsets.only(left: 5.0, right: 5.0),
-            ),
-            ListTile(
-              title: new Text(
-                'Log Out',
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              onTap: () async {
-                await _auth.signOut();
-                Navigator.of(context).pushReplacementNamed('/loginpage');
-              },
+              onPressed: () => _pickImage(ImageSource.gallery),
+              color: Colors.pink,
             ),
           ],
         ),
       ),
-      body: Text("Verify"),
+      body: ListView(
+        children: <Widget>[
+          if (_imageFile != null) ...[
+            Container(
+                padding: EdgeInsets.all(32), child: Image.file(_imageFile)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                FlatButton(
+                  color: Colors.black,
+                  child: Icon(Icons.crop),
+                  onPressed: _cropImage,
+                ),
+                FlatButton(
+                  color: Colors.black,
+                  child: Icon(Icons.refresh),
+                  onPressed: _clear,
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(32),
+              child: Uploader(
+                file: _imageFile,
+              ),
+            )
+          ]
+        ],
+      ),
     );
+  }
+}
+
+/// Widget used to handle the management of
+class Uploader extends StatefulWidget {
+  final File file;
+
+  Uploader({Key key, this.file}) : super(key: key);
+
+  createState() => _UploaderState();
+}
+
+class _UploaderState extends State<Uploader> {
+  final FirebaseStorage _storage =
+      FirebaseStorage(storageBucket: 'gs://bay-max-5311c.appspot.com');
+
+  StorageUploadTask _uploadTask;
+
+  _startUpload() {
+    String filePath = 'images/${DateTime.now()}.png';
+
+    setState(() {
+      _uploadTask = _storage.ref().child(filePath).putFile(widget.file);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_uploadTask != null) {
+      return StreamBuilder<StorageTaskEvent>(
+          stream: _uploadTask.events,
+          builder: (context, snapshot) {
+            var event = snapshot?.data?.snapshot;
+
+            double progressPercent = event != null
+                ? event.bytesTransferred / event.totalByteCount
+                : 0;
+
+            return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  if (_uploadTask.isComplete)
+                    Text('🎉🎉🎉',
+                        style: TextStyle(
+                            color: Colors.greenAccent,
+                            height: 2,
+                            fontSize: 30)),
+                  if (_uploadTask.isPaused)
+                    FlatButton(
+                      child: Icon(Icons.play_arrow, size: 50),
+                      onPressed: _uploadTask.resume,
+                    ),
+                  if (_uploadTask.isInProgress)
+                    FlatButton(
+                      child: Icon(Icons.pause, size: 50),
+                      onPressed: _uploadTask.pause,
+                    ),
+                  LinearProgressIndicator(value: progressPercent),
+                  Text(
+                    '${(progressPercent * 100).toStringAsFixed(2)} % ',
+                    style: TextStyle(fontSize: 50),
+                  ),
+                ]);
+          });
+    } else {
+      return FlatButton.icon(
+          color: Colors.blue,
+          label: Text('Upload to Firebase'),
+          icon: Icon(Icons.cloud_upload),
+          onPressed: _startUpload);
+    }
   }
 }
