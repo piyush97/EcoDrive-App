3680cfd24b1db013520988cb5640cf416c249700
Piyush Mehta <me@piyushmehta.com>
2019-09-09 13:36:30 +0530

    sidebar

--- lib/pages/bikes.dart
+++ lib/pages/bikes.dart
@@ -1,3 +1,4 @@
+import 'package:ecodrive/pages/verify_profile.dart';
 import 'package:flutter/material.dart';
 import "../pages/plans.dart";
 import "../pages/rides.dart";
@@ -48,7 +49,8 @@ class _BikesPageState extends State<BikesPage> {
                 Navigator.push(
                     context,
                     new MaterialPageRoute(
-                        builder: (BuildContext context) => new Rides()));
+                        builder: (BuildContext context) =>
+                            new VerifyProfile()));
               },
             ),
             ListTile(
