ef2fdb0391f5f487b511f3d31c37e2a03c708db2
piyush97 <me@piyushmehta.com>
2019-12-30 00:26:51 +0530

    Fix

--- lib/pages/rentNow.dart
+++ lib/pages/rentNow.dart
@@ -97,33 +97,3 @@ class BookNowState extends State<BookNow> {
     return "$hoursStr:$minutesStr:$secondsStr";
   }
 }
-
-// class BookNow extends StatelessWidget {
-//   Widget ecobikeImage() {
-//     return Image(image: AssetImage('assets/ecobike.png'), fit: BoxFit.contain);
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       margin: EdgeInsets.symmetric(horizontal: 5),
-//       padding: EdgeInsets.all(20),
-//       decoration: new BoxDecoration(
-//         borderRadius: new BorderRadius.circular(10),
-//         color: Colors.black87,
-//       ),
-//       child: Column(
-//         mainAxisSize: MainAxisSize.min,
-//         children: <Widget>[
-//           ecobikeImage(),
-//           Text(
-//             'Ride Starts Now',
-//             style: TextStyle(
-//               color: Colors.white70,
-//             ),
-//           )
-//         ],
-//       ),
-//     );
-//   }
-// }
