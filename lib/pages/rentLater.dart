d4994a7bf2452070e40c179e82efe7ca4e700ad9
piyush97 <me@piyushmehta.com>
2019-12-30 00:42:44 +0530

    Payment

--- lib/pages/rentLater.dart
+++ lib/pages/rentLater.dart
@@ -2,7 +2,9 @@ import 'package:ecodrive/pages/plans.dart';
 import 'package:flutter/material.dart';
 import 'package:date_range_picker/date_range_picker.dart' as DateRangePicker;
 import 'package:intl/intl.dart';
-import 'dart:math';
+import 'package:razorpay_flutter/razorpay_flutter.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:flutter/services.dart';
 
 class Selectdate extends StatefulWidget {
   @override
@@ -13,6 +15,7 @@ class _SelectdateState extends State<Selectdate> {
   GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   DateTime startDate;
   DateTime endDate;
+  static const platform = const MethodChannel("razorpay_flutter");
 
   DateFormat formatDate = DateFormat('dd/MM/yyyy');
 
@@ -35,6 +38,57 @@ class _SelectdateState extends State<Selectdate> {
     'ECO40',
     'ECO50',
   ];
+
+  Razorpay _razorpay;
+  @override
+  void dispose() {
+    super.dispose();
+    _razorpay.clear();
+  }
+
+  void openCheckout2500() async {
+    var options = {
+      'key': 'rzp_test_1DP5mmOlF5G5ag',
+      'amount': noOfDays * basePrice,
+      'name': 'Eco Drive',
+      'description': 'Eco Drive Payment',
+      'prefill': {'contact': '8888888888', 'email': 'test@razorpay.com'},
+      'external': {
+        'wallets': ['paytm']
+      }
+    };
+
+    try {
+      _razorpay.open(options);
+    } catch (e) {
+      debugPrint(e);
+    }
+  }
+
+  void _handlePaymentSuccess(PaymentSuccessResponse response) {
+    Fluttertoast.showToast(
+        msg: "SUCCESS: " + response.paymentId, timeInSecForIos: 4);
+  }
+
+  void _handlePaymentError(PaymentFailureResponse response) {
+    Fluttertoast.showToast(
+        msg: "ERROR: " + response.code.toString() + " - " + response.message,
+        timeInSecForIos: 4);
+  }
+
+  void _handleExternalWallet(ExternalWalletResponse response) {
+    Fluttertoast.showToast(
+        msg: "EXTERNAL_WALLET: " + response.walletName, timeInSecForIos: 4);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _razorpay = Razorpay();
+    _razorpay.on(Razorpay.EVENT_PAYMENT_SUCCESS, _handlePaymentSuccess);
+    _razorpay.on(Razorpay.EVENT_PAYMENT_ERROR, _handlePaymentError);
+    _razorpay.on(Razorpay.EVENT_EXTERNAL_WALLET, _handleExternalWallet);
+  }
 
   // @override
   // void initState() {
@@ -478,11 +532,7 @@ class _SelectdateState extends State<Selectdate> {
       );
       return;
     }
-
-    Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => PlansPage()),
-    );
+    openCheckout2500();
   }
 
   int basePrice = 250;
